d8cbc53f586bb67403759a9967ae836207e01356
apoorvcodes <tofu.develops@gmail.com>
2022-04-08 15:46:28 +0530

    feat: get rid of godotenv

--- main.go
+++ main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"log"
+
 	"os"
 
 	"github.com/TeamIndex/Backend/router"
 	"github.com/gominima/cors"
 	middleware "github.com/gominima/middlewares"
 	"github.com/gominima/minima"
-	"github.com/joho/godotenv"
+
 )
 
 func main() {
-	Init()
+
 	app := minima.New()
 	app.Use(cors.New().AllowAll())
 	app.UseRaw(middleware.Logger)
@@ -27,12 +27,6 @@ func main() {
 	app.Listen(":"+GetENV("PORT"))
 }
 
-func Init() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-}
 
 func GetENV(key string) string {
 	return os.Getenv(key)
